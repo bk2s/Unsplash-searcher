9be2c9fc16d5659aec6eeb7aa77c28e329e68785
bk2s <bk2scom@gmail.com>
2021-10-15 15:21:47 +0300

    Initial Commit

---
+++ Unsplash searcher/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Unsplash searcher
+//
+//  Created by  Stepanok Ivan on 15.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
